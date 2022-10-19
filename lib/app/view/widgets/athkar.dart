83c144327aac73211b13fbe466eec34f57a90ac3
realhassan97 <therealhassan97@gmail.com>
2022-10-19 15:31:24 +0300

    new widget

---
+++ lib/app/view/widgets/athkar.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class MyAthkar extends StatelessWidget {
+  final String title;
+  final IconData icon;
+  final double height;
+
+  const MyAthkar({
+    required this.title,
+    required this.icon,
+    required this.height,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(25),
+      width: 175,
+      height: height,
+      decoration: BoxDecoration(
+        color: Theme.of(context).primaryColor,
+        borderRadius: const BorderRadius.all(
+          Radius.circular(20),
+        ),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Icon(icon),
+          Text(title),
+        ],
+      ),
+    );
+  }
+}
