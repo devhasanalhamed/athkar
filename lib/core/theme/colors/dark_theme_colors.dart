fd2395a3b9cd1f629ca68d599feff7bb3d7ac22a
realhassan97 <therealhassan97@gmail.com>
2022-11-11 21:31:02 +0300

    complete the api to bring the prayer time information for riyadh

--- lib/core/theme/colors/dark_theme_colors.dart
+++ lib/core/theme/colors/dark_theme_colors.dart
@@ -4,4 +4,12 @@ class ColorsDark {
   static const primaryColor = Color.fromARGB(255, 85, 110, 83);
   static const backgroundColor = Color.fromARGB(255, 255, 255, 255);
   static const textColorBody = Color.fromARGB(255,0,0,0);
+
+  static const textColorBodyMedium = primaryColor;
+  static const textColorTitleSmall = backgroundColor;
+  static const textColorHeadlineMedium = primaryColor;
+  static const textColorBodySmall = backgroundColor;
+
+  static const iconColor = backgroundColor;
+  static const splashColor = backgroundColor;
 }
--- lib/core/theme/data/dark_theme_data.dart
+++ lib/core/theme/data/dark_theme_data.dart
@@ -5,5 +5,83 @@ ThemeData getDarkThemeData() => ThemeData(
   brightness: Brightness.light,
   primaryColor: ColorsDark.primaryColor,
   backgroundColor: ColorsDark.backgroundColor,
+  fontFamily: 'NotoKufiArabic',
+  textTheme: appTextTheme,
+  appBarTheme: const AppBarTheme(
+    color: ColorsDark.primaryColor,
+    foregroundColor: ColorsDark.backgroundColor,
+    elevation: 0.0,
+    iconTheme: IconThemeData(
+        size: 25
+    ),
+  ),
+
+  floatingActionButtonTheme: const FloatingActionButtonThemeData(
+    foregroundColor: ColorsDark.backgroundColor,
+    backgroundColor: ColorsDark.primaryColor,
+  ),
+  elevatedButtonTheme: ElevatedButtonThemeData(
+    style: ButtonStyle(
+      backgroundColor: MaterialStateProperty.all(ColorsDark.primaryColor),
+      foregroundColor: MaterialStateProperty.all(ColorsDark.backgroundColor),
+    ),
+  ),
+  splashColor: ColorsDark.splashColor,
+
+  iconTheme: const IconThemeData(
+    color: ColorsDark.iconColor,
+    size: 45,
+  ),
+  chipTheme: const ChipThemeData(
+    backgroundColor: ColorsDark.primaryColor,
+  ),
+  buttonTheme: const ButtonThemeData(
+    buttonColor: ColorsDark.primaryColor,
+  ),
+);
+
+
+
+
+TextTheme appTextTheme = const TextTheme(
+  headlineMedium: TextStyle(
+    fontSize: 28,
+    color: ColorsDark.textColorHeadlineMedium,
+    overflow: TextOverflow.fade,
+    fontWeight: FontWeight.bold,
+  ),
+
+
+  titleSmall: TextStyle(
+    fontSize: 18,
+    color: ColorsDark.textColorTitleSmall,
+    overflow: TextOverflow.fade,
+    fontWeight: FontWeight.bold,
+  ),
+  titleMedium: TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.bold,
+    color: ColorsDark.textColorBodyMedium,
+    overflow: TextOverflow.fade,
+  ),
+
+
+  bodyLarge: TextStyle(
+    fontSize: 24,
+    color: ColorsDark.textColorBodyMedium,
+    overflow: TextOverflow.fade,
+  ),
+  bodyMedium: TextStyle(
+    fontSize: 18,
+    color: ColorsDark.textColorBodyMedium,
+    overflow: TextOverflow.fade,
+    fontWeight: FontWeight.bold,
+  ),
+  bodySmall: TextStyle(
+    fontSize: 14,
+    color: ColorsDark.textColorBodySmall,
+    overflow: TextOverflow.fade,
+    fontWeight: FontWeight.normal,
+  ),
 
 );
