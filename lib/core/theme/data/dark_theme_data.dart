91e0a2215d6fde66768003a860fd2e06bd676400
realhassan97 <therealhassan97@gmail.com>
2022-11-15 05:50:10 +0300

    editing some dark theme data

--- lib/core/theme/data/dark_theme_data.dart
+++ lib/core/theme/data/dark_theme_data.dart
@@ -2,7 +2,7 @@ import 'package:athkar/core/theme/colors/dark_theme_colors.dart';
 import 'package:flutter/material.dart';
 
 ThemeData getDarkThemeData() => ThemeData(
-  brightness: Brightness.light,
+  brightness: Brightness.dark,
   primaryColor: ColorsDark.primaryColor,
   backgroundColor: ColorsDark.backgroundColor,
   fontFamily: 'NotoKufiArabic',
