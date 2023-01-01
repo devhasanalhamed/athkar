45b171130837e0b4388a2c749de39bc08383a0ce
Hassan Al-Hamed <therealhassan97@gmail.com>
2023-01-01 17:01:19 +0300

    smallTitle
    change the font size of the small title

--- lib/core/theme/data/light_theme_data.dart
+++ lib/core/theme/data/light_theme_data.dart
@@ -2,46 +2,40 @@ import 'package:athkar/core/theme/colors/light_theme_colors.dart';
 import 'package:flutter/material.dart';
 
 ThemeData getLightThemeData() => ThemeData(
-  brightness: Brightness.light,
-  primaryColor: ColorsLight.primaryColor,
-  backgroundColor: ColorsLight.backgroundColor,
-  fontFamily: 'NotoKufiArabic',
-  textTheme: appTextTheme,
-  appBarTheme: const AppBarTheme(
-    color: ColorsLight.primaryColor,
-    foregroundColor: ColorsLight.backgroundColor,
-    elevation: 0.0,
-    iconTheme: IconThemeData(
-      size: 25
-    ),
-  ),
-
-  floatingActionButtonTheme: const FloatingActionButtonThemeData(
-    foregroundColor: ColorsLight.backgroundColor,
-    backgroundColor: ColorsLight.primaryColor,
-  ),
-  elevatedButtonTheme: ElevatedButtonThemeData(
-    style: ButtonStyle(
-      backgroundColor: MaterialStateProperty.all(ColorsLight.primaryColor),
-      foregroundColor: MaterialStateProperty.all(ColorsLight.backgroundColor),
-    ),
-  ),
-  splashColor: ColorsLight.splashColor,
-
-  iconTheme: const IconThemeData(
-    color: ColorsLight.iconColor,
-    size: 45,
-  ),
-  chipTheme: const ChipThemeData(
-    backgroundColor: ColorsLight.primaryColor,
-  ),
-  buttonTheme: const ButtonThemeData(
-    buttonColor: ColorsLight.primaryColor,
-  ),
-);
-
-
-
+      brightness: Brightness.light,
+      primaryColor: ColorsLight.primaryColor,
+      backgroundColor: ColorsLight.backgroundColor,
+      fontFamily: 'NotoKufiArabic',
+      textTheme: appTextTheme,
+      appBarTheme: const AppBarTheme(
+        color: ColorsLight.primaryColor,
+        foregroundColor: ColorsLight.backgroundColor,
+        elevation: 0.0,
+        iconTheme: IconThemeData(size: 25),
+      ),
+      floatingActionButtonTheme: const FloatingActionButtonThemeData(
+        foregroundColor: ColorsLight.backgroundColor,
+        backgroundColor: ColorsLight.primaryColor,
+      ),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all(ColorsLight.primaryColor),
+          foregroundColor:
+              MaterialStateProperty.all(ColorsLight.backgroundColor),
+        ),
+      ),
+      splashColor: ColorsLight.splashColor,
+      iconTheme: const IconThemeData(
+        color: ColorsLight.iconColor,
+        size: 45,
+      ),
+      chipTheme: const ChipThemeData(
+        backgroundColor: ColorsLight.primaryColor,
+      ),
+      buttonTheme: const ButtonThemeData(
+        buttonColor: ColorsLight.primaryColor,
+      ),
+    );
 
 TextTheme appTextTheme = const TextTheme(
   headlineMedium: TextStyle(
@@ -50,10 +44,8 @@ TextTheme appTextTheme = const TextTheme(
     overflow: TextOverflow.fade,
     fontWeight: FontWeight.bold,
   ),
-
-
   titleSmall: TextStyle(
-    fontSize: 18,
+    fontSize: 16,
     color: ColorsLight.textColorTitleSmall,
     overflow: TextOverflow.fade,
     fontWeight: FontWeight.bold,
@@ -64,8 +56,6 @@ TextTheme appTextTheme = const TextTheme(
     color: ColorsLight.textColorBodyMedium,
     overflow: TextOverflow.fade,
   ),
-
-
   bodyLarge: TextStyle(
     fontSize: 24,
     color: ColorsLight.textColorBodyMedium,
@@ -83,5 +73,4 @@ TextTheme appTextTheme = const TextTheme(
     overflow: TextOverflow.fade,
     fontWeight: FontWeight.normal,
   ),
-
 );
